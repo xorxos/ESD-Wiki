eb761e99317331062f223596731c74f2e4b2dad2
Parker Leavitt <parkertleavitt@gmail.com>
2019-03-27 16:15:23 -0500

    add component specific html div's

--- src/app/articles/team-wiki/create-article/create-article.component.ts
+++ src/app/articles/team-wiki/create-article/create-article.component.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit } from '@angular/core'
-import { IArticle, ITitleSection, IRichTextSection } from '../../shared/article.model';
+import { IArticle, 
+         ITitleSection, 
+         IRichTextSection, 
+         IStepDividerSection, 
+         IImageLeftSection, 
+         IImageRightSection, 
+         IFullWidthImageSection, 
+         IBulletedListSection, 
+         INumberedListSection } from '../../shared/article.model';
 
 @Component({
     templateUrl: 'create-article.component.html',
@@ -12,6 +20,12 @@ export class CreateArticleComponent implements OnInit {
     newArticle: IArticle
     titleSection: ITitleSection
     richTextSection: IRichTextSection
+    stepDividerSection: IStepDividerSection
+    imageLeftSection: IImageLeftSection
+    imageRightSection: IImageRightSection
+    fullWidthImageSection: IFullWidthImageSection
+    bulletedListSection: IBulletedListSection
+    numberedListSection: INumberedListSection
 
     ngOnInit() {
         this.showConfiguration = true
@@ -30,17 +44,7 @@ export class CreateArticleComponent implements OnInit {
         }
     }
 
-    isTitleSection(content): boolean {
-        if(content.selector === "Title") {
-            return true
-        } else return false
-    }
-
-    isRichTextSection(content): boolean {
-        if(content.selector === "RichText") {
-            return true
-        } else return false
-    }
+    /** Functions to handle which menu is being shown */
 
     toggleAddSection() {
         this.showAddSection = true
@@ -60,12 +64,118 @@ export class CreateArticleComponent implements OnInit {
         this.showConfiguration = true
     }
 
+    /** Functions to check which component is in newArticle.articleContents */
+
+    isTitleComponent(component): boolean {
+        if(component.selector === "Title") {
+            return true
+        } else return false
+    }
+
+    isRichTextComponent(component): boolean {
+        if(component.selector === "RichText") {
+            return true
+        } else return false
+    }
+
+    isStepDividerComponent(component): boolean {
+        if(component.selector === "Step Divider") {
+            return true
+        } else return false
+    }
+
+    isBulletedListComponent(component): boolean {
+        if(component.selector === "Bulleted List") {
+            return true
+        } else return false
+    }
+
+    isNumberedListComponent(component): boolean {
+        if(component.selector === "Numbered List") {
+            return true
+        } else return false
+    }
+
+    isImageLeftComponent(component): boolean {
+        if(component.selector === "Image-left With Text") {
+            return true
+        } else return false
+    }
+
+    isImageRightComponent(component): boolean {
+        if(component.selector === "Image-right With Text") {
+            return true
+        } else return false
+    }
+
+    isFullWidthImageComponent(component): boolean {
+        if(component.selector === "Full-Width Image") {
+            return true
+        } else return false
+    }
+
+    /** Functions to add components */
+
     addRichTextComponent() {
         this.richTextSection = {
             selector: "RichText",
             contents: "This is some rich text"
         }
         this.newArticle.articleContents.push(this.richTextSection)
+        this.toggleConfigurationSection()
+    }
+
+    addStepDividerComponent() {
+        this.stepDividerSection = {
+            selector: "Step Divider",
+            contents: "Step 1"
+        }
+        this.newArticle.articleContents.push(this.stepDividerSection)
+        this.toggleConfigurationSection()
+    }
+
+    addBulletedListComponent() {
+        this.bulletedListSection = {
+            selector: "Bulleted List",
+            contents: "Step 1"
+        }
+        this.newArticle.articleContents.push(this.bulletedListSection)
+        this.toggleConfigurationSection()
+    }
+
+    addNumberedListComponent() {
+        this.numberedListSection = {
+            selector: "Numbered List",
+            contents: "Step 1"
+        }
+        this.newArticle.articleContents.push(this.numberedListSection)
+        this.toggleConfigurationSection()
+    }
+
+    addImageLeftComponent() {
+        this.imageLeftSection = {
+            selector: "Image-left With Text",
+            contents: "This is some rich text"
+        }
+        this.newArticle.articleContents.push(this.imageLeftSection)
+        this.toggleConfigurationSection()
+    }
+
+    addImageRightComponent() {
+        this.imageRightSection = {
+            selector: "Image-right With Text",
+            contents: "This is some rich text"
+        }
+        this.newArticle.articleContents.push(this.imageRightSection)
+        this.toggleConfigurationSection()
+    }
+
+    addFullWidthImageComponent() {
+        this.fullWidthImageSection = {
+            selector: "Full-Width Image",
+            contents: "This is some rich text"
+        }
+        this.newArticle.articleContents.push(this.fullWidthImageSection)
         this.toggleConfigurationSection()
     }
 }
