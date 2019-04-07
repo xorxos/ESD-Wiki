945939639da46dc0f77d69238b7a8180a83b95e4
Parker Leavitt <parkertleavitt@gmail.com>
2019-04-07 18:06:05 -0500

    redesign articleList by category & team wiki pages

--- src/app/articles/shared/article.service.ts
+++ src/app/articles/shared/article.service.ts
@@ -1,19 +1,19 @@
 import { Injectable } from '@angular/core'
-import  { IArticle } from './article.model'
+import { IArticle } from './article.model'
 
 @Injectable()
 export class ArticleService {
-    articleList:IArticle[]
+    articleList: IArticle[]
 
-    getArticleByCategory(category:string):IArticle[] {
+    getArticleByCategory(category: string): IArticle[] {
         return this.articleListByCategory(category)
     }
 
-    getAllArticles(){
+    getAllArticles() {
         return ARTICLES
     }
 
-    articleListByCategory(category:string):IArticle[] {
+    articleListByCategory(category: string): IArticle[] {
         this.articleList = []
         ARTICLES.forEach(item => {
             if (item.categoryTags.includes(category)) {
@@ -22,11 +22,20 @@ export class ArticleService {
         })
         console.log(this.articleList)
         return this.articleList
-        
+    }
+
+    getTeamArticleByCategory(category:string): IArticle[] {
+        this.articleList = []
+        TEAMARTICLES.forEach(item => {
+            if (item.categoryTags.includes(category)) {
+                this.articleList.push(item)
+            }
+        })
+        return this.articleList
     }
 }
 
-const ARTICLES:IArticle[] = [
+const ARTICLES: IArticle[] = [
     {
         id: 1,
         title: "Windows - How to Sign-in to Skype",
@@ -62,4 +71,43 @@ const ARTICLES:IArticle[] = [
         articleContents: [],
         categoryTags: ["Skype", "Mobile Devices"]
     },
+]
+
+const TEAMARTICLES: IArticle[] = [
+    {
+        id: 1,
+        title: "How to add members to a security group",
+        description: "This is a step-by-step guide on how to add or remove members of a security group in active directory.",
+        articleContents: [],
+        categoryTags: ["Active Directory", "PC"]
+    },
+    {
+        id: 2,
+        title: "VPN security group memberships",
+        description: "This article will walk you through which memberships are required for VPN access.",
+        articleContents: [],
+        categoryTags: ["Active Directory", "Mac"]
+    },
+    {
+        id: 3,
+        title: "How to change the manager on an employee account",
+        description: "This is a guide on how to change the manager of an employee account in active directory.",
+        articleContents: [],
+        categoryTags: ["Active Directory", "Mobile Devices"]
+    },
+    {
+        id: 4,
+        title: "How to find a bitlocker key",
+        description: "This guide will walk you through how to find a bitlocker key in active directory",
+        articleContents: [],
+        categoryTags: ["Active Directory", "Mobile Devices"]
+    },
+    {
+        id: 5,
+        title: "How to add an employee ID",
+        description: "This is a step-by-step guide on how to add an employee ID to an active directory account.",
+        articleContents: [],
+        categoryTags: ["Active Directory", "Mobile Devices"]
+    },
+
 ]
--- src/app/articles/team-wiki/team-wiki.component.ts
+++ src/app/articles/team-wiki/team-wiki.component.ts
@@ -1,18 +1,19 @@
-import { Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
+import { ArticleService } from '../shared/article.service'
+import { IArticle } from '../shared/article.model';
 
 @Component({
     selector: 'team-wiki',
     templateUrl: './team-wiki.component.html',
     styleUrls: ['./team-wiki.component.css']
 })
-export class TeamWikiComponent {
-    HideCreateArticleButton:boolean = true;
+export class TeamWikiComponent implements OnInit {
+    articleList:IArticle[]
 
-    mouseEntered() {
-        this.HideCreateArticleButton = false;
+    constructor(private ArticleService: ArticleService) {
     }
 
-    mouseLeft() {
-        this.HideCreateArticleButton = true;
+    ngOnInit(): void {
+        this.articleList = this.ArticleService.getTeamArticleByCategory("Active Directory")
     }
 }
